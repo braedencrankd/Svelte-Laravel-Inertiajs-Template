0139e6cc82a3351118b5bdb86e7f7430e6863141
braedencrankd <braeden@crankd.nz>
2022-03-08 14:30:59 +1300

    Setting up auth

---
+++ resources/js/Components/Button.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let text = "";
+  export let enabled = true;
+</script>
+
+<button type on:click class:active={enabled} class={$$props.class}>
+  {text}
+</button>
+
+<style lang="postcss">
+  button {
+    @apply rounded bg-gray-400 py-2 px-4 font-bold text-white;
+  }
+
+  .active {
+    @apply bg-blue-500;
+  }
+
+  .active:hover {
+    @apply bg-blue-600;
+  }
+</style>
---
+++ resources/js/Pages/Login.svelte
@@ -0,0 +1,11 @@
+<script>
+  import LoginForm from "../Components/LoginForm.svelte";
+
+  import Card from "../Components/Card.svelte";
+</script>
+
+<main class="container mx-auto">
+  <Card>
+    <LoginForm />
+  </Card>
+</main>
